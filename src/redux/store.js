138c3d1b7a97a2d36d9c875c127d17741e953d1d
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-07 19:13:31 +0300

    +

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,38 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { createSlice, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import filterReducer from './filterSlice';
 
 const persistConfig = {
-key: 'root',
-storage,
+  key: 'root',
+  storage,
 };
 
 const contactsSlice = createSlice({
-name: 'contacts',
-initialState: [],
-reducers: {
-addContact: (state, action) => {
-state.push(action.payload);
-},
-deleteContact: (state, action) => {
-return state.filter((contact) => contact.id !== action.payload);
-},
-},
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      return state.filter((contact) => contact.id !== action.payload);
+    },
+  },
 });
 
 const persistedContactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
 
 const rootReducer = {
-contacts: persistedContactsReducer,
-filter: filterReducer,
+  contacts: persistedContactsReducer,
+  filter: filterReducer,
 };
 
+const middleware = [...getDefaultMiddleware({ serializableCheck: false })];
+
 export const store = configureStore({
-reducer: rootReducer,
+  reducer: rootReducer,
+  middleware,
 });
 
 export const persistor = persistStore(store);
